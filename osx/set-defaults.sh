1a4543f72a2afcf3180d045240e5a56e31bced24
Penn Su <pennsu@gmail.com>
2013-12-15 12:28:58 +0800

    Time machine tweaks

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -324,6 +324,15 @@ open "${DOTFILES}/files/Wez.itermcolors"
 open "${DOTFILES}/files/Wombat.itermcolors"
 open "${DOTFILES}/files/Zenburn.itermcolors"
 
+###############################################################################
+# Time Machine                                                                #
+###############################################################################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 ###############################################################################
 # Address Book, Dashboard, iCal, TextEdit, and Disk Utility                   #

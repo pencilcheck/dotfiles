e9bf0cbee35dc526edadf2720cb21ec2af7cb364
Penn Su <pennsu@gmail.com>
2013-12-15 12:31:00 +0800

    MAS changes

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -349,7 +349,15 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
+###############################################################################
+# Mac App Store                                                               #
+###############################################################################
 
+# Enable the WebKit Developer Tools in the Mac App Store
+defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+# Enable Debug Menu in the Mac App Store
+defaults write com.apple.appstore ShowDebugMenu -bool true
 
 ###############################################################################
 # Kill affected applications                                                  #

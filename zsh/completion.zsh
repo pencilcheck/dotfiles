9e1891f4b15d897cf4a56cde43b6045db64b0268
Penn Su <pennsu@gmail.com>
2018-08-27 13:04:52 -0700

    lunchy completion is used to manage launchctl scripts

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -6,3 +6,9 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+
+LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
+if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
+  . $LUNCHY_DIR/lunchy-completion.zsh
+fi

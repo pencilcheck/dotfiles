a7333e214d1fe16954c737e21ad14e06b160c63d
Penn Su <pennsu@gmail.com>
2018-08-27 13:03:58 -0700

    Not using docker now

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,3 +1,2 @@
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/penn/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+# Docker (default for Vagrant based boxes)
+#export DOCKER_HOST=tcp://localhost:2375

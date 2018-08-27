288baee2c4206b816b6d03a2c341d2358d9cf287
Penn Su <pennsu@gmail.com>
2018-08-27 13:05:52 -0700

    Added some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,8 @@
-alias ls='ls -G'
-alias lsal='ls -alG'
+alias ls='exa'
+alias lsal='exa -al'
+#alias ls='ls -G'
+#alias lsal='ls -alG'
 alias vpi='nvim +PlugUpgrade +PlugInstall +qall'
 alias vci='nvim +PlugClean +qall'
 alias ssh='ssh-add -l || ssh-add && ssh'
+alias love="/Applications/love.app/Contents/MacOS/love"

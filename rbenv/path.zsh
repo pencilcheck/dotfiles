78537d536222f705063dbf6f03fe62f91437803f
Penn Su <pennsu@gmail.com>
2018-08-27 13:03:40 -0700

    Add rbenv paths

--- rbenv/path.zsh
+++ rbenv/path.zsh
@@ -1,4 +1,4 @@
 # for Rbenv
-export PATH=~/.rbenv/shims:$PATH
-export PATH=~/.rbenv/bin:$PATH
-export PATH=~/.rbenv/plugins/ruby-build/bin:$PATH
+export PATH="/.rbenv/shims:$PATH"
+export PATH="/.rbenv/bin:$PATH"
+export PATH="/.rbenv/plugins/ruby-build/bin:$PATH"
